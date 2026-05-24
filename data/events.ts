d7d9d53e521430c84ad9c9c7144d7d77a70daadc
sharmunerasmus <sharmun.erasmus@gmail.com>
2026-05-24 03:06:48 +0200

    Moved source data to a shared events.ts file

---
+++ data/events.ts
@@ -0,0 +1,56 @@
+export const Sunday24thAMRoster = {
+  Title: 'Sunday 24th AM',
+  dateTime: new Date('2026-05-24T07:30:00'),
+  CallTime: '7:30am',
+  StartTime: '9:00am',
+  roster: [
+  { role: 'Sound Engineer', name: 'Buddy/Natanya' },
+  { role: 'Lights/Steaming', name: 'Cole' },
+  { role: 'Lyrics/Scripture', name: 'Faith/Tyrone' },
+  { role: 'Switching', name: 'Nel-Mari' },
+  { role: 'Camera 1', name: 'Keagan' },
+  { role: 'Camera 2', name: 'Faith/Xavier' },
+  { role: 'Camera 3', name: 'Luke' },
+  { role: 'Handheld Camera', name: 'Sharmun' }
+],
+}
+
+export const Sunday24thPMRoster = {
+  Title: 'Sunday 24th PM',
+  dateTime: new Date('2026-05-24T16:30:00'),
+  CallTime: '4:30pm',
+  StartTime: '6:00pm',
+  roster: [
+  { role: 'Sound Engineer', name: 'Kudzai/Sharmun' },
+  { role: 'Lights/Steaming', name: 'Cole' },
+  { role: 'Lyrics/Scripture', name: 'Sharmun/Faith' },
+  { role: 'Switching', name: 'Sharmun/Cuan' },
+  { role: 'Camera 1', name: 'Keagan' },
+  { role: 'Camera 2', name: '?/Static' },
+  { role: 'Camera 3', name: 'Luke' },
+  { role: 'Handheld Camera', name: 'N/A' }
+],
+}
+
+export const Monday25thPMRoster = {
+  Title: 'Monday 25th PM',
+  dateTime: new Date('2026-05-25T18:00:00'),
+  CallTime: '6:00pm',
+  StartTime: '6:30pm',
+  roster: [
+  { role: 'Sound Engineer', name: '?' },
+  { role: 'Lights/Steaming', name: '?' },
+  { role: 'Lyrics/Scripture', name: '?' },
+  { role: 'Switching', name: '?' },
+  { role: 'Camera 1', name: '?' },
+  { role: 'Camera 2', name: '?' },
+  { role: 'Camera 3', name: '?' },
+  { role: 'Handheld Camera', name: 'N/A' }
+],
+}
+
+export const events = [
+  Sunday24thAMRoster,
+  Sunday24thPMRoster,
+  Monday25thPMRoster,
+];
